6f006a0616201b7858c747fc7428c8e5e3bd86a0
seitosugino <0jb4f61k3853v2f@gmail.com>
2022-02-04 23:04:17 +0900

    準備

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import java.lang.reflect.Array
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnAdd:Button = findViewById(R.id.btnAdd)
+        val lv:ListView = findViewById(R.id.lv)
+
+        val adapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            mutableListOf()
+        )
+        lv.adapter = adapter
     }
 }
